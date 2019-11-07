6e2d18948fb7ee9e75cb9f0ee718a97cf3dc6913
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-11-07 09:48:38 -0500

    updated train script

--- train.bat
+++ train.bat
@@ -4,6 +4,14 @@ echo "Are you sure you want to do this?"
 echo "Press Ctrl C to cancel"
 pause
 
+for /L %%C in (550, 50, 600) DO (
+    for /L %%D in (200, 50, 400) DO (
+        echo "Training for DBP: 300, OPENING_DIST: 190, A: %%C, JUMP_V: %%D . . ."
+        python train.py 300 190 %%C %%D 75
+    )
+)
+
+
 for /L %%A in (350, 50, 400) DO (
     for /L %%B in (150, 40, 200) DO (
         for /L %%C in (300, 50, 600) DO (
